d73e0b323e8ec0c89f811e4b063312d0bc601274
Namratha.Prabhanjana <namratha.prabhanjana@gmail.com>
2024-10-09 03:33:10 -0700

    cgroup usage

---
+++ runq-latency/runq-latency-cgroup.py
@@ -0,0 +1,28 @@
+from bcc import BPF
+import sys
+from time import sleep
+
+# Attach the bpf program
+b = BPF(src_file="runq_latency_cgroup.c")
+#b.attach_tracepoint(tp="sched:sched_switch", fn_name="trace_sched_switch")
+#b.attach_tracepoint(tp="sched:sched_enqueue_task", fn_name="trace_sched_enqueue_task")
+#b.attach_tracepoint(tp="sched:sched_process_exit", fn_name="trace_sched_process_exit")
+
+b.attach_kprobe(event="finish_task_switch", fn_name="trace_sched_switch")
+
+# define the callback for perf buffer
+def print_event(cpu, data, size):
+    event = b["events"].event(data)
+    print("PID: %d, comm: %s, runq latency: %d" % (event.pid, event.comm, event.latency))
+
+b["events"].open_perf_buffer(print_event)
+print("Tracing runq latency...")
+# loop with callback to print_event
+try:
+    while True:
+        b.perf_buffer_poll()
+except KeyboardInterrupt:
+    print("Exiting...")
+    sys.exit(0)
+except Exception as e: 
+    print(str(e))
